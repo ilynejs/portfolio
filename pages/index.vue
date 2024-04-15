f6e56cd2d77b34f36076f5a443b1e434029e875e
Ruben DC <rubendcdev@proton.me>
2024-04-15 14:15:15 +0200

    Ajout de la page d'accueil avec le composant Storyblok

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+	const story = await useStoryblok('home', { version: 'draft' });
+</script>
+
+<template>
+	<StoryblokComponent v-if="story" :blok="story.content" />
+</template>
+
+<style lang="scss" scoped></style>
