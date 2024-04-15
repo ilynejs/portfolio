3c69a840c7b5162147e1d11619520684686a434c
Ruben DC <rubendcdev@proton.me>
2024-04-15 14:15:41 +0200

    Mise à jour du composant Page.vue avec le support de Storyblok

---
+++ storyblok/Page.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+	defineProps({ blok: Object });
+</script>
+
+<template>
+	<div class="page" v-editable="blok">
+		<StoryblokComponent
+			v-for="blok in blok.body"
+			:key="blok._uid"
+			:blok="blok"
+		/>
+	</div>
+</template>
+
+<style lang="scss">
+	.page {
+		display: flex;
+		flex-direction: column;
+		gap: 120px;
+
+		margin: 120px 0;
+	}
+</style>
