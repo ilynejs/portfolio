54943d7a7b07cadd8b9887e9ad0face1a4363047
Ruben DC <rubendcdev@proton.me>
2024-04-15 14:16:44 +0200

    Ajout des composants Storyblok Contact et Contacts

---
+++ storyblok/Contact.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+	const props = defineProps<{
+		blok: {
+			icon: string;
+			label: string;
+			link: {
+				url: URL;
+				target: string;
+			};
+		};
+	}>();
+</script>
+
+<template>
+	<NuxtLink class="contact" :to="blok.link.url" :target="blok.link?.target">
+		<div class="contact__icon">
+			<Icon :name="blok.icon" size="32px" />
+		</div>
+
+		<h3 class="contact__label">{{ blok.label }}</h3>
+	</NuxtLink>
+</template>
+
+<style lang="scss" scoped>
+	.contact {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		flex-wrap: nowrap;
+		gap: 10px;
+
+		width: fit-content;
+		padding: 5px 30px 5px 5px;
+		border-radius: 10px;
+
+		transition: background 0.25s ease;
+		// user-select: none;
+
+		&:hover {
+			background: $bg-pill;
+		}
+
+		&__icon {
+			display: flex;
+			align-items: center;
+			justify-content: center;
+
+			aspect-ratio: 1;
+			width: 30px;
+			height: 30px;
+
+			padding: 5px;
+			border-radius: 5px;
+
+			background: $bg-icon;
+		}
+
+		&__label {
+			font-weight: 400;
+			font-size: 13px;
+		}
+	}
+</style>
---
+++ storyblok/Contacts.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+	const props = defineProps<{
+		blok: object;
+	}>();
+</script>
+
+<template>
+	<section v-editable="blok" class="contact">
+		<h2 class="title">Me contacter</h2>
+
+		<div class="contact__links">
+			<StoryblokComponent
+				v-for="contact in blok.contacts"
+				:key="contact._uid"
+				:blok="contact"
+			/>
+		</div>
+	</section>
+</template>
+
+<style lang="scss" scoped>
+	.title {
+		font-size: 24px;
+	}
+
+	.contact {
+		display: flex;
+		flex-direction: column;
+		gap: 35px;
+
+		width: 100%;
+
+		&__links {
+			display: flex;
+			flex-wrap: wrap;
+			gap: 40px;
+
+			width: 100%;
+		}
+	}
+</style>
